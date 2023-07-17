d115237bc5eab4265ce555cdeb59a2f504682477
avmochalov <al.mochalov93@gmail.com>
2023-07-17 17:37:18 +0300

    track list from data source

---
+++ src/data.js
@@ -0,0 +1,14 @@
+export const tracks = [
+    { title: 'Guilt', info: '', author: 'Nero', album: 'Welcome Reality', time: '4:44' },
+    { title: 'Elektro', info: '', author: 'Dynoro, Outwork, Mr. Gee', album: 'Elektro', time: '2:22' },
+    { title: 'I’m Fire', info: '', author: 'Ali Bakgor', album: 'I’m Fire', time: '2:22' },
+    { title: 'Non Stop',info: '(Remix)', author: 'Стоункат, Psychopath', album: 'Non Stop', time: '4:12' },
+    { title: 'Run Run',info: '(feat. AR/CO)', author: 'Jaded, Will Clarke, AR/CO', album: 'Run Run' , time: '2:54'},
+    { title: 'Eyes on Fire',info: '(Zeds Dead Remix)', author: 'Blue Foundation, Zeds Dead', album: 'Eyes on Fire', time: '5:20' },
+    { title: 'Mucho Bien',info: '(Hi Profile Remix)', author: 'HYBIT, Mr. Black, Offer Nissim, Hi Profile', album: 'Mucho Bien', time: '3:41' },
+    { title: 'Knives n Cherries',info: '', author: 'minthaze', album: 'Captivating', time: '1:48' },
+    { title: 'How Deep Is Your Love',info: '', author: 'Calvin Harris, Disciples', album: 'How Deep Is Your Love', time: '3:32' },
+    { title: 'Morena',info: '', author: 'Tom Boxer', album: 'Soundz Made in Romania', time: '3:36' },
+    { title: '',info: '', author: '', album: '', time: '' },
+];
+console.log(tracks[0].title);
