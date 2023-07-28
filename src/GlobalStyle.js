5b2f80bdafa109c618c383e47f5918845ed4bc77
avmochalov <al.mochalov93@gmail.com>
2023-07-28 17:51:24 +0300

    Global Styles

---
+++ src/GlobalStyle.js
@@ -0,0 +1,107 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+  *:before,
+  *:after {
+    box-sizing: border-box;
+  }
+  
+  a,
+  a:visited {
+    text-decoration: none;
+    font-family: 'StratosSkyeng', sans-serif;
+    cursor: pointer;
+  }
+  
+  button,
+  ._btn {
+    cursor: pointer;
+  }
+  
+  ul li {
+    list-style: none;
+  }
+  
+  //-------------------------------//
+  
+  // ========== fonts ========
+  @font-face {
+    font-family: 'StratosSkyeng';
+    src:
+      local('StratosSkyeng'),
+      local('StratosSkyeng'),
+      url('../public/fonts/Stratos-Regular.woff2') format('woff2'),
+      url('../public/fonts/Stratos-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+  }
+  
+  //--------------------------
+  
+  html,
+  body {
+    width: 100%;
+    height: 100%;
+    font-family: 'StratosSkyeng', sans-serif;
+    color: #ffffff;
+  }
+  
+  // ========== hover ========
+  ._btn-text:hover {
+    border-color: #d9b6ff;
+    color: #d9b6ff;
+    cursor: pointer;
+  }
+  
+  ._btn-icon:hover svg {
+    fill: transparent;
+    stroke: #acacac;
+    cursor: pointer;
+  }
+  
+  // ========== activ ========
+  
+  ._btn-text:active {
+    border-color: #ad61ff;
+    color: #ad61ff;
+    cursor: pointer;
+  }
+  
+  ._btn-icon:active svg {
+    fill: transparent;
+    stroke: #ffffff;
+    cursor: pointer;
+  }
+  
+  ._btn-icon:active .track-play__like-svg,
+  ._btn-icon:active .track-play__dislike-svg {
+    fill: #696969;
+    stroke: #ffffff;
+    cursor: pointer;
+  }
+  
+  // =================== main ====================//
+  
+  .search {
+    &__text {
+  
+    }
+  }
+  
+  .filter {
+    &__button {
+      &_clicked {
+        border-color: #d9b6ff;
+        color: #d9b6ff;
+      }
+    }
+  }
+  
+  
+`;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './style.css';
-import App from './App';
+import App from './components/App/App';
+import { GlobalStyle } from './GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
-
